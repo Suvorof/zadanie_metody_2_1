ddf56966b2373716387dd67f429c85ddb89b3cf8
Konstantin Suvorov <konsu@inbox.ru>
2018-05-09 18:59:46 +0300

    Добавьте файлы проекта.

---
+++ zadanie_metody_2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_metody_2_1
+{
+    class Program
+    {
+        static int wholecredit = 700;
+
+        static void Banking(int payment)
+        {
+            wholecredit -= payment;
+            if (wholecredit == 0)
+            {
+                Console.WriteLine("кредит погашен");
+            }
+            else if (wholecredit != 0 && wholecredit > 0)
+            {
+                Console.WriteLine("Вам осталось внести: {0}", wholecredit);
+                Console.WriteLine();
+            }
+            if (wholecredit < 0)
+            {
+                Console.WriteLine("Переплата составляе: {0}", wholecredit *  (-1));
+            }
+        }
+
+        static void Main()
+        {
+            Banking(200);
+            Banking(300);
+            Banking(400);
+            Console.ReadKey();
+        }
+    }
+}
